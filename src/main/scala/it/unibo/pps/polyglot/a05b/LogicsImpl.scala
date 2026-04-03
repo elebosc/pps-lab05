c35ca40ad87e897dab8a2c2c2c971a2e4492f263
Elena Boschetti <elena.boschetti@studio.unibo.it>
2026-04-03 18:20:07 +0200

    [t2] replace Pair case class with tuple

--- src/main/scala/it/unibo/pps/polyglot/a05b/LogicsImpl.scala
+++ src/main/scala/it/unibo/pps/polyglot/a05b/LogicsImpl.scala
@@ -7,38 +7,36 @@ import scala.util.Random
 /** solution and descriptions at https://bitbucket.org/mviroli/oop2019-esami/src/master/a05b/sol2/ */
 class LogicsImpl(private val size: Int) extends Logics:
 
-  private case class Pair[X, Y](x: X, y: Y)
-
   private val random: Random = Random
-  private val initial: Pair[Int, Int] = Pair(randomPosition, randomPosition)
+  private val (xi, yi) = (randomPosition, randomPosition)
   private var tickCount: Int = 0
 
   private def randomPosition: Int = random.nextInt(size - 2) + 1
 
   override def tick(): Unit = tickCount += 1
-  
+
   private def isWithinCurrentRange(delta: Int): Boolean = Math.abs(delta) <= tickCount
 
   private def isOnCurrentVerticalAxis(x: Int, y: Int): Boolean =
-    val isOnVerticalAxis = x == initial.x
-    isOnVerticalAxis && isWithinCurrentRange(y - initial.y)
+    val isOnVerticalAxis = x == xi
+    isOnVerticalAxis && isWithinCurrentRange(y - yi)
 
   private def isOnCurrentHorizontalAxis(x: Int, y: Int): Boolean =
-    val isOnHorizontalAxis = y == initial.y
-    isOnHorizontalAxis && isWithinCurrentRange(x - initial.x)
+    val isOnHorizontalAxis = y == yi
+    isOnHorizontalAxis && isWithinCurrentRange(x - xi)
 
   private def isOnCurrentForwardDiagonal(x: Int, y: Int): Boolean =
-    val isOnForwardDiagonal = x - y == initial.x - initial.y
-    isOnForwardDiagonal && isWithinCurrentRange(x - initial.x)
+    val isOnForwardDiagonal = x - y == xi - yi
+    isOnForwardDiagonal && isWithinCurrentRange(x - xi)
 
   private def isOnCurrentBackwardDiagonal(x: Int, y: Int): Boolean =
-    val isOnBackwardDiagonal = x + y == initial.x + initial.y
-    isOnBackwardDiagonal && isWithinCurrentRange(x - initial.x)
+    val isOnBackwardDiagonal = x + y == xi + yi
+    isOnBackwardDiagonal && isWithinCurrentRange(x - xi)
 
   override def hasElement(x: Int, y: Int): Boolean =
-    isOnCurrentVerticalAxis(x, y) || isOnCurrentHorizontalAxis(x, y) || 
+    isOnCurrentVerticalAxis(x, y) || isOnCurrentHorizontalAxis(x, y) ||
     isOnCurrentForwardDiagonal(x, y) || isOnCurrentBackwardDiagonal(x, y)
 
   private def isOutOfBound(p: Int): Boolean = p - tickCount < 0 || p + tickCount >= size
 
-  override def isOver: Boolean = isOutOfBound(initial.x) || isOutOfBound(initial.y)
+  override def isOver: Boolean = isOutOfBound(xi) || isOutOfBound(yi)
