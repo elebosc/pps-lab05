0fff41872fc58cbe1cd199717a0e85f9ea7bdc21
Elena Boschetti <elena.boschetti@studio.unibo.it>
2026-04-03 17:13:54 +0200

    [t2] add logic implementation for a05b

--- src/main/scala/it/unibo/pps/polyglot/a05b/LogicsImpl.scala
+++ src/main/scala/it/unibo/pps/polyglot/a05b/LogicsImpl.scala
@@ -2,10 +2,27 @@ package it.unibo.pps.polyglot.a05b
 
 import it.unibo.pps.polyglot.a05b.Logics
 
+import scala.util.Random
+
 /** solution and descriptions at https://bitbucket.org/mviroli/oop2019-esami/src/master/a05b/sol2/ */
 class LogicsImpl(private val size: Int) extends Logics:
-  override def tick(): Unit = {}
 
-  override def isOver: Boolean = false
+  private case class Pair[X, Y](x: X, y: Y)
 
-  override def hasElement(x: Int, y: Int): Boolean = false
+  private val random: Random = Random
+  private val initial: Pair[Int, Int] = Pair(randomPosition, randomPosition)
+  private var tickCount: Int = 0
+
+  private def randomPosition: Int = random.nextInt(size - 2) + 1
+
+  override def tick(): Unit = tickCount = tickCount + 1
+
+  override def hasElement(x: Int, y: Int): Boolean =
+    (x == initial.x && Math.abs(y - initial.y) <= tickCount) ||
+    (y == initial.y && Math.abs(x - initial.x) <= tickCount) ||
+    (x - y == initial.x - initial.y && Math.abs(x - initial.x) <= tickCount) ||
+    (x + y == initial.x + initial.y && Math.abs(x - initial.x) <= tickCount)
+
+  private def isOutOfBound(p: Int) = p - tickCount < 0 || p + tickCount >= size
+
+  override def isOver: Boolean = isOutOfBound(initial.x) || isOutOfBound(initial.y)
