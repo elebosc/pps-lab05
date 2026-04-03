306100021d01385dd6acebfd571a48fedc8a6171
Elena Boschetti <elena.boschetti@studio.unibo.it>
2026-04-03 17:44:07 +0200

    [t2] improve hasElement readibility

--- src/main/scala/it/unibo/pps/polyglot/a05b/LogicsImpl.scala
+++ src/main/scala/it/unibo/pps/polyglot/a05b/LogicsImpl.scala
@@ -15,13 +15,22 @@ class LogicsImpl(private val size: Int) extends Logics:
 
   private def randomPosition: Int = random.nextInt(size - 2) + 1
 
-  override def tick(): Unit = tickCount = tickCount + 1
+  override def tick(): Unit = tickCount += 1
+
+  private def isOnVerticalAxis(x: Int, y: Int): Boolean =
+    x == initial.x && Math.abs(y - initial.y) <= tickCount
+
+  private def isOnHorizontalAxis(x: Int, y: Int): Boolean =
+    y == initial.y && Math.abs(x - initial.x) <= tickCount
+
+  private def isOnForwardDiagonal(x: Int, y: Int): Boolean =
+    x - y == initial.x - initial.y && Math.abs(x - initial.x) <= tickCount
+
+  private def isOnBackwardDiagonal(x: Int, y: Int): Boolean =
+    x + y == initial.x + initial.y && Math.abs(x - initial.x) <= tickCount
 
   override def hasElement(x: Int, y: Int): Boolean =
-    (x == initial.x && Math.abs(y - initial.y) <= tickCount) ||
-    (y == initial.y && Math.abs(x - initial.x) <= tickCount) ||
-    (x - y == initial.x - initial.y && Math.abs(x - initial.x) <= tickCount) ||
-    (x + y == initial.x + initial.y && Math.abs(x - initial.x) <= tickCount)
+    isOnVerticalAxis(x, y) || isOnHorizontalAxis(x, y) || isOnForwardDiagonal(x, y) || isOnBackwardDiagonal(x, y)
 
   private def isOutOfBound(p: Int) = p - tickCount < 0 || p + tickCount >= size
 
